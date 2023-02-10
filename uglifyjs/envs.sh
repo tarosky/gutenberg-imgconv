246d27b05f58ff71528a38aed5b1e32d27a834cc
Akihiro Harai <jharai0815@gmail.com>
2023-02-10 16:40:46 +0900

    Update UglifyJS and Node.js #41

--- uglifyjs/envs.sh
+++ uglifyjs/envs.sh
@@ -1,4 +1,4 @@
 export USER_ID="$(id -u)"
 export GROUP_ID="$(id -g)"
-export NODEJS_VERSION="${NODEJS_VERSION:-18.14.0}"
+export NODEJS_VERSION="${NODEJS_VERSION:-14.15.3}"
 export UGLIFYJS_VERSION="${UGLIFYJS_VERSION:-3.17.4}"
